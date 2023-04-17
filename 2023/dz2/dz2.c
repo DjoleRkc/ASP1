6b0533094a29f7c5078476bab6c713098dc3d813
Djole <Djole>
2023-04-18 01:37:30 +0200

    updated version of homework 2

--- 2023/dz2/dz2.c
+++ 2023/dz2/dz2.c
@@ -4,7 +4,7 @@
 #include <string.h>
 #define _CRT_SECURE_NO_WARNINGS
 
-int n, m;
+int n, m, sizeOfRules;
 
 typedef struct node
 {
@@ -97,15 +97,89 @@ void levelOrder(Node* root) {
 }
 
 void printMatrix(char*** matrix) {
-	for (int i = 0; i < m+1; i++)
+	for (int i = 0; i < m; i++)
 	{
 		for (int j = 0; j < n; j++) printf("%s ", matrix[i][j]);
 		putchar('\n');
 	}
 }
 
+char*** readFile(FILE* file, char*** matrix) {
+	char** tmp = malloc(300*sizeof(char));
+	fscanf(file, "%d\n%d\n", &n, &m);
+	for (int i = 0; i < m; i++) {
+		matrix[i] = malloc(n * sizeof(char*));
+		fscanf(file, "%s", tmp);
+		char** token = strtok(tmp, ",");
+		for (int j = 0; j < n; j++)
+		{
+			matrix[i][j] = malloc(100*sizeof(char));
+			strcpy(matrix[i][j], token);
+			matrix[i][j] = realloc(matrix[i][j], strlen(matrix[i][j]) + 1);
+			token = strtok(NULL, ",");
+		}
+	}
+	char*** rules = malloc(50 * sizeof(char**));
+	sizeOfRules = 0;
+	rules[0] = malloc(3 * sizeof(char*));
+	rules[0][0] = malloc(100 * sizeof(char));
+	rules[0][1] = malloc(100 * sizeof(char));
+	rules[0][2] = malloc(2 * sizeof(char));
+	while (fscanf(file, "%[^ +-]%c%[^\n]\n", rules[sizeOfRules][0], rules[sizeOfRules][2], rules[sizeOfRules][1]) == 3)
+	{
+		rules[sizeOfRules][0] = realloc(rules[sizeOfRules][0], strlen(rules[sizeOfRules][0]) + 1);
+		rules[sizeOfRules][1] = realloc(rules[sizeOfRules][1], strlen(rules[sizeOfRules][1]) + 1);
+		rules[sizeOfRules][2][1] = '\0';
+		rules[++sizeOfRules] = malloc(2 * sizeof(char*));
+		rules[sizeOfRules][0] = malloc(100 * sizeof(char));
+		rules[sizeOfRules][1] = malloc(100 * sizeof(char));
+		rules[sizeOfRules][2] = malloc(2 * sizeof(char));
+	}
+	return rules;
+}
+
+char*** readConsole(char*** matrix) {
+	char** tmp = malloc(300 * sizeof(char));
+	scanf("%d\n%d\n", &n, &m);
+	for (int i = 0; i < m; i++) {
+		matrix[i] = malloc(n * sizeof(char*));
+		for (int j = 0; j < n; j++)
+		{
+			matrix[i][j] = malloc(100 * sizeof(char));
+			scanf("%s", matrix[i][j]);
+			matrix[i][j] = realloc(matrix[i][j], strlen(matrix[i][j]) + 1);
+		}
+	}
+	char*** rules = malloc(50 * sizeof(char**));
+	char** tmp2 = malloc(100 * sizeof(char));
+	sizeOfRules = 0;
+	getchar();
+	while (scanf("%[^ \n]", tmp2) == 1)
+	{
+		getchar();
+		rules[sizeOfRules] = malloc(2 * sizeof(char*));
+		rules[sizeOfRules][0] = malloc(100 * sizeof(char));
+		rules[sizeOfRules][1] = malloc(100 * sizeof(char));
+		rules[sizeOfRules][2] = malloc(2 * sizeof(char));
+		sscanf(tmp2, "%[^ +-]%c%s", rules[sizeOfRules][0], rules[sizeOfRules][2], rules[sizeOfRules][1]);
+		rules[sizeOfRules][0] = realloc(rules[sizeOfRules][0], strlen(rules[sizeOfRules][0]) + 1);
+		rules[sizeOfRules][1] = realloc(rules[sizeOfRules][1], strlen(rules[sizeOfRules][1]) + 1);
+		rules[sizeOfRules++][2][1] = '\0';
+		
+	}
+	return rules;
+}
+
 int main() {
-	n = 3, m = 2;
+	FILE* f = fopen("test33-ok.txt", "r");
+	char*** matrix = malloc((m + 1) * sizeof(char**));
+	//char*** rules = readFile(f, matrix);
+	char*** rules = readConsole(matrix);
+	for (size_t i = 0; i < sizeOfRules; i++)
+	{
+		printf("%s %s %s\n", rules[i][0], rules[i][1], rules[i][2]);
+	}
+	/*n = 3, m = 2;
 	char*** matrix = malloc((m + 1) * sizeof(char**));
 	for (int i = 0; i < m + 1; i++) {
 		matrix[i] = malloc(n * sizeof(char*));
@@ -116,15 +190,5 @@ int main() {
 			matrix[i][j] = realloc(matrix[i][j], strlen(matrix[i][j]) + 1);
 		}
 	}
-	printMatrix(matrix);
-	/*char*** new = copyState(matrix);
-	new[2][0] = "la";
-	printMatrix(new);
 	printMatrix(matrix);*/
-
-	/*char* s = malloc(20*sizeof(char));
-	scanf("%s", s);
-	s = realloc(s, strlen(s) + 1);
-	printf("%s %d", s, strlen(s));
-	free(s);*/
 }
